478080b2805a4a15f7eb589ff66c74d5dac2d5ee
Chris Sanchez <c.sanch7@gmail.com>
2020-02-28 08:37:28 -0800

    clean up code, add FontAwesome, add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../App.css';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h1><i class="fas fa-book"></i> Book Search</h1>
+        </header>
+    )
+}
+
+export default Header;
