79797d36bb2d8f7c5a6ef78ace638664ea2e4cbe
Chris Sanchez <c.sanch7@gmail.com>
2020-02-28 09:49:19 -0800

    fix indentation

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,45 +4,45 @@ import request from 'superagent';
 import SearchArea from './SearchArea';
 
 class Books extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			books: [],
-			searchField: ''
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      searchField: ''
+    }
+  }
 
-	searchBook = (e) => {
-		e.preventDefault();
-		request
-			.get("https://www.googleapis.com/books/v1/volumes")
-			.query({ q: this.state.searchField })
-			.then((data) => {
-				const books = data.body.items
-				for (let i = 0; i < 10; i++) {
-					const bookTitles = books[i].volumeInfo.title
-					console.log(bookTitles);
+  searchBook = (e) => {
+    e.preventDefault();
+    request
+      .get("https://www.googleapis.com/books/v1/volumes")
+      .query({ q: this.state.searchField })
+      .then((data) => {
+        const books = data.body.items
+        for (let i = 0; i < 10; i++) {
+          const bookTitles = books[i].volumeInfo.title
+          console.log(bookTitles);
 
-				}
-				console.log(data);
-				console.log(books);
-			})
-	}
+        }
+        console.log(data);
+        console.log(books);
+      })
+  }
 
-	handleSearch = (e) => {
-		this.setState({ searchField: e.target.value })
-	}
+  handleSearch = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
 
-	render() {
-		return (
-			<div>
-				<SearchArea
-					handleSearch={this.handleSearch}
-					searchBook={this.searchBook}
-				/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <SearchArea
+          handleSearch={this.handleSearch}
+          searchBook={this.searchBook}
+        />
+      </div>
+    );
+  }
 }
 
 export default Books;
