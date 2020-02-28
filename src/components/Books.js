7c73f142828e82530b4f2957a6935fef36900076
Chris Sanchez <c.sanch7@gmail.com>
2020-02-28 11:35:06 -0800

    method manageResponseProperties checks if properties exist, such as missing thumbnails, maps the response data, and returns a cleaned up version- this is then the new state

--- src/components/Books.js
+++ src/components/Books.js
@@ -20,8 +20,9 @@ class Books extends Component {
       .get("https://www.googleapis.com/books/v1/volumes")
       .query({ q: this.state.searchField })
       .then((data) => {
-        // console.log(data.body.items[0]);
-        this.setState({ books: [...data.body.items] })
+        const cleanData = this.manageResponseProperties(data)
+        // able to pass cleanData into books instead of the spread of 'data.body.items' because it is a managed response in a mapped format
+        this.setState({ books: cleanData })
       })
   }
 
@@ -32,6 +33,21 @@ class Books extends Component {
   handleSort = (e) => {
     this.setState({ sort: e.target.value })
     console.log(e.target.value);
+  }
+
+  manageResponseProperties = (data) => {
+    const cleanData = data.body.items.map((book) => {
+      if (book.volumeInfo.hasOwnProperty('publishedDate') === false) {
+        // this value is temporary while working on sort functionality
+        book.volumeInfo['publishedDate'] = '0000';
+      } else if (book.volumeInfo.hasOwnProperty('imageLinks') === false) {
+        // if the imageLinks.thumbnail property doesn't exist, one is created passing in a hosted image of 'BOOK COVER NOT AVAILABLE' from imgur
+        book.volumeInfo['imageLinks'] = { thumbnail: 'https://i.imgur.com/J5LVHEL.jpg' }
+      }
+      return book;
+    })
+    // new, managed response from mapping
+    return cleanData;
   }
 
   render() {
