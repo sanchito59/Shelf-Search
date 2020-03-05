6a03f299f7728c61dcc5a68d5d9ffde6bb3546a4
Chris Sanchez <c.sanch7@gmail.com>
2020-03-05 09:33:30 -0800

    add error handling

--- src/components/NYTBestsellers.js
+++ src/components/NYTBestsellers.js
@@ -4,22 +4,28 @@ import './../App.css';
 import BestsellerList from './BestsellerList';
 
 export default function NYTBestsellers(props) {
-  console.log('nyt props: ', props)
-  return (
-    <div>
-      <h1>This Week's NYT Bestsellers: </h1>
-      {
-        props.bestSellers.map((book, i) => {
-          return <BestsellerList
-            title={book.book_details[0].title}
-            author={book.book_details[0].author}
-            currentRank={book.rank}
-            rankLastWeek={book.rank_last_week}
-            weeksOnList={book.weeks_on_list}
-            key={i}
-          />
-        })
-      }
-    </div>
-  )
+  // console.log('nyt props: ', props.bestSellers)
+  if (typeof props.bestSellers !== 'undefined') {
+    return (
+      <div>
+        <h1>This Week's NYT Bestsellers: </h1>
+        {
+          props.bestSellers.map((book, i) => {
+            return <BestsellerList
+              title={book.book_details[0].title}
+              author={book.book_details[0].author}
+              currentRank={book.rank}
+              rankLastWeek={book.rank_last_week}
+              weeksOnList={book.weeks_on_list}
+              key={i}
+            />
+          })
+        }
+      </div>
+    )
+  } else {
+    return (
+      <div>NYT Bestsellers unavailable.</div>
+    );
+  }
 }
