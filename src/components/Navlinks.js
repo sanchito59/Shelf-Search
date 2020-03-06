e99f645c1b1fc5389c7fbdf55509e567aa16d3d8
Chris Sanchez <c.sanch7@gmail.com>
2020-03-06 13:50:02 -0800

    add basic navbar style

--- src/components/Navlinks.js
+++ src/components/Navlinks.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// Styles/Assets
+import './../Navlinks.css';
 
 export default function Navlinks() {
   return (
-    <div>
+    <div class='navlinks'>
       <Link to='/'>Home</Link> | <Link to='poemOfDay'>Poetry</Link> | <Link to='/bookSearch'>Search for Books</Link>
     </div>
   );
