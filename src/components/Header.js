05f0ce5e874b9ac8ef5db5228eab7ca3b2dc2a81
Chris Sanchez <c.sanch7@gmail.com>
2020-03-06 12:49:39 -0800

    update modules, relaunch server, add working, ugly routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
+// Functionality
 import React from 'react';
+import { Link } from 'react-router-dom';
+// Styles/Assets
 import './../App.css';
 
-const Header = () => {
+function Header() {
   return (
     <header className="App-header">
       <h1><i className="fas fa-book"></i> Book Search</h1>
+      <Link to='/'>Home</Link> | <Link to='poemOfDay'>Poetry</Link> | <Link to='/bookSearch'>Search for Books</Link>
     </header>
   )
 }
