cc38514cb15b420a59c60e0e500f0bb8c9b8661a
Chris Sanchez <c.sanch7@gmail.com>
2020-03-06 14:12:59 -0800

    render API call data

--- src/components/PoemOfDay.js
+++ src/components/PoemOfDay.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './../PoemOfDay.css';
 
 export default function PoemOfDay(props) {
+  // async API call hasn't passed the data since before the component mounts?
   setTimeout(() => {
+    // logging 3 times?
     if (typeof (props.poem.poem) === 'undefined') {
-      // logging 3 times?
       console.log('need to mount?');
       // debugger;
     } else {
@@ -16,14 +17,12 @@ export default function PoemOfDay(props) {
 
   return (
     <div className="poem-div">
-      <h2 className="poem-title">a...synchronous</h2>
-      <h4 className="poem-author">by code sinner</h4>
-      <p className="poem-body">poem body <br></br>
-        test section <br></br>
-        goes here, <br></br>
-        but lives in <br></br>
-        console debug hell
-      </p>
+      {/* This works, but if you change 'props.poem.description' it breaks */}
+      <h1 className="poem-of-the-day">{props.poem.description}</h1>
+      <h2 className="poem-title">{typeof props.poem.description === 'undefined' ? 'Test Title' : props.poem.poem.title}</h2>
+      <h4 className="poem-author">{typeof props.poem.description === 'undefined' ? 'Test Author' : props.poem.poem.author}</h4>
+      {/* needs to be processed and displayed as poetry */}
+      <p className="poem-body">{typeof props.poem.description === 'undefined' ? 'Test Author' : props.poem.poem.poem}</p>
     </div>
   );
 }
