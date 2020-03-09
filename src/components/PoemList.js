a7aeeaf4aabf50bc431b748027d4c481416ba9a5
Chris Sanchez <c.sanch7@gmail.com>
2020-03-09 15:13:53 -0700

    basic poem display

--- src/components/PoemList.js
+++ src/components/PoemList.js
@@ -4,18 +4,23 @@ import SinglePoemDisplay from './SinglePoemDisplay';
 
 export default function PoemList(props) {
   // console.log('PoemList props: ', props);
-  return (
-    <div>
-      {
-        props.poemList.map((poem, i) => {
-          return <SinglePoemDisplay
-            title={poem.title}
-            author={poem.author}
-            poem={poem.lines}
-            key={i}
-          />
-        })
-      }
-    </div>
-  );
+  if (props.poemList.length > -1) {
+    return (
+      <div>
+        {
+          props.poemList.map((poem, i) => {
+            console.log(poem.lines)
+            return <SinglePoemDisplay
+              title={poem.title}
+              author={poem.author}
+              poem={poem.lines}
+              key={i}
+            />
+          })
+        }
+      </div>
+    );
+  } else {
+    return <p>Couldn't find anything, try searching again!</p>
+  }
 }
--- src/components/SinglePoemDisplay.js
+++ src/components/SinglePoemDisplay.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
 export default function SinglePoemDisplay(props) {
-  console.log('singlePoemDisplay props: ', props);
+  // console.log('singlePoemDisplay props: ', props);
+  const poemDiv = {
+    textAlign: 'left',
+    marginLeft: '30px'
+  }
+  const listStyle = {
+    listStyle: 'none',
+  }
+
+  const poemAuthor = {
+    textTransform: 'uppercase',
+    textDecoration: 'underline',
+    letterSpacing: '1px'
+  }
+
+  const poemBody = {
+    fontFamily: 'Caladea, sans-serif',
+  }
+
   return (
-    <div></div>
+    <div style={poemDiv}>
+      <h2>{props.title}</h2>
+      <h4>BY <span style={poemAuthor}>{props.author}</span></h4>
+      <div style={poemBody}>
+        {
+          props.poem.map((line, i) => {
+            console.log(i, ': ', line)
+            return <li style={listStyle}>{line}</li>
+          })
+        }
+      </div>
+    </div>
   );
 }
