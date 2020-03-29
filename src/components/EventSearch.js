219a167ea2daefca6f2ad65ad2ac76ff24288e5b
Chris Sanchez <c.sanch7@gmail.com>
2020-03-29 14:01:53 -0700

    convert to styled components

--- src/components/EventSearch.js
+++ src/components/EventSearch.js
@@ -1,19 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 // Styles/Assets
 import "./../App.css";
+import searchIcon from './../assets/searchicon.png'
+
+const SearchInput = styled.input`
+  text-indent: 10px;
+  padding: 8px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  background-image: url(${searchIcon});
+  background-position: 2.5px 5px;
+  background-repeat: no-repeat;
+    &:hover {
+      border: 2px solid rgb(151, 139, 139);
+    }
+`
+
+const SearchButton = styled.button`
+    background-color: rgb(204, 186, 107);
+    border-radius: 8px;
+    border: none;
+    color: white;
+    padding: 7.5px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2), 0 2px 16px 0 rgba(0, 0, 0, 0.19);
+    -webkit-transition-duration: 0.4s;
+    /* Safari */
+    transition-duration: 0.4s;
+      &:hover {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.24), 0 12px 10px 0 rgba(0, 0, 0, 0.19);
+      }
+  `
 
 export default function EventSearch(props) {
   return (
     <div className="search-area">
       <form onSubmit={props.eventSearch}>
         <label>Zip Code: </label>
-        <input
+        <SearchInput
           className="search-input"
           placeholder="97204"
           onChange={props.handleEventSearch}
           type="text"
-        ></input>
-        <button className='search-button'>Search for Events</button>
+        />
+        <SearchButton>Search for Events</SearchButton>
       </form>
     </div>
   );
--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,21 +1,57 @@
 import React from "react";
+import styled from 'styled-components';
 // Components
 import BookList from './BookList'
 // Styles/Assets
 import "./../App.css";
+import searchIcon from './../assets/searchicon.png'
 
+const SearchInput = styled.input`
+  text-indent: 10px;
+  padding: 8px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  background-image: url(${searchIcon});
+  background-position: 2.5px 5px;
+  background-repeat: no-repeat;
+    &:hover {
+      border: 2px solid rgb(151, 139, 139);
+    }
+`
+
+const SearchButton = styled.button`
+    background-color: rgb(204, 186, 107);
+    border-radius: 8px;
+    border: none;
+    color: white;
+    padding: 7.5px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2), 0 2px 16px 0 rgba(0, 0, 0, 0.19);
+    -webkit-transition-duration: 0.4s;
+    /* Safari */
+    transition-duration: 0.4s;
+      &:hover {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.24), 0 12px 10px 0 rgba(0, 0, 0, 0.19);
+      }
+  `
 const SearchArea = (props) => {
   // console.log('search props: ', props)
   return (
     <div className="search-area">
       <form action="" onSubmit={props.searchForBooks}>
-        <input
-          className="search-input"
+        <SearchInput
           onChange={props.handleSearch}
           type="text"
           placeholder="cats, sci-fi, Absurdist philosophy etc."
-        ></input>
-        <button type="submit" className='search-button'>Search</button>
+        />
+        <SearchButton>
+          Search
+        </SearchButton>
         <input type="checkbox" id="ebook-param" value="ebook-param" onChange={props.handleEbookFilter}></input>
         <label htmlFor="ebook-param">e-book </label>
         {/* add logic to only show when booklist mounts? */}
